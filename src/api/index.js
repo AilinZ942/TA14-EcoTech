0c4b97d81fff2c9d20e4a7d68b691081f3321fa1
PENG CHENGWEI <pcw@PENGs-MacBook-Air.local>
2026-04-11 20:52:58 +1000

    hardcode url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_BASE
+const BASE = "https://ta14-ecotech-backend-ecf9e5hca9fpf7da.australiaeast-01.azurewebsites.net/api"
 
 export const api = {
   getPerson: async () => {
