694c49daeba56bfdc2fc6bb206603b4687b411f3
Ailin Zou <azou0002@student.monash.edu>
2026-04-28 11:06:09 +1000

    Hide uncomplete page

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
       <router-link to="/">Home</router-link>
       <router-link to="/dashboard">Dashboard</router-link>
       <!--<router-link to="/repair-check">Repair Check</router-link> -->
-      <router-link to="/extend-usage">Extend Usage</router-link>
+      <!--<router-link to="/extend-usage">Extend Usage</router-link> -->
       <router-link to="/device-optimizer">AI Device Optimizer</router-link>
-      <router-link to="/safe-guidance">Safe Guidance</router-link>
+      <!--<router-link to="/safe-guidance">Safe Guidance</router-link>-->
       <router-link to="/disposal-locations">Disposal Locations</router-link>
     </div>
   </nav>
